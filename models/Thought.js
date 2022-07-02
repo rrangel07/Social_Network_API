cf0e1097881b82a4f626d187efec43ab0e9432a3
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-07-01 20:56:33 -0400

    Thought model generated

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+const Reaction = require('./Reaction');
+
+const thoughtsSchema = new Schema ({
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: val => moment(val).format('L [@] LT'),
+        },
+        username: {
+            type: String,
+            require: true,
+        },
+        reactions: [Reaction],
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+});
+
+thoughtsSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('user',userSchema);
+
+module.exports = Thought;
